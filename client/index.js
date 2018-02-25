edc25e4434dfa7f1a11bf69d9d702e5d5eda7647
benjaminr177 <unoceptik@gmail.com>
2018-02-25 11:41:23 +0200

    basic webpack setup, created client side with react

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/style.css'
+
+const App = () => {
+  return <h1>Wassup</h1>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
